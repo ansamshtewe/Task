01697fbae85de42623e8b37ee6c5f565d9e5ffb3
DELL <DELL@DESKTOP-76LQSM5>
2020-02-28 21:18:47 +0200

    commit

--- src/software/test/TestCalc.java
+++ src/software/test/TestCalc.java
@@ -13,18 +13,7 @@ public class TestCalc {
 		Calc obj=new Calc();
 	}
 	
-	@Test
-	public void testSum_BothNumbersArePositive_ShouldReturnPositiveNumber() {
-	// Arrange
-	int a = 10;
-	int b = 20;
-	Calc calc = new Calc();
-	// Act
-	int result = calc.add(a, b);
-	// Assert
-	Assert.assertTrue(result > 0);
-	}
-
+	
 	@Test
 	public void test0() { 
 		
