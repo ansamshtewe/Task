53c9dc055709434b99554ecffb7ce617c437878a
DELL <DELL@192.168.1.8>
2020-03-02 19:49:43 +0200

    vvff

--- src/software/java/Calc.java
+++ src/software/java/Calc.java
@@ -1,12 +1,14 @@
 package software.java;
 
 public class Calc {
-    public int add(int x,int y)
+    public static int add(int x,int y)
     {
     	return x+y;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println(add(2,3));
 
 	}
 
